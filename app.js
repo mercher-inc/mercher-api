94e75830b52506616b55dd8994ce0f6445b46d85
Dmitry Les <dmitriy.s.les@gmail.com>
2014-07-31 13:00:38 +0300

    sockets

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+app.set('port', 3001);
 
-// view engine setup
+var server = require('http').createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+var io = require('socket.io')(server);
+
+// view engine setupl
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -27,8 +31,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,27 +62,33 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+        res.json({
+            error: {
+                message: err.message,
+                error:   err
+            }
         });
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    res.json({
+        error: {
+            message: err.message
+        }
     });
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function (socket) {
+    console.log('a user connected', socket.id/*, socket.request.headers*/);
+    socket.on('disconnect', function(){
+        console.log('user disconnected', socket.id);
+    });
 });
 
 module.exports = app;
